52b5efca7a06c1376878b5de7722fce085f88d9d
Pedro <jpedro1000@gmail.com>
2024-07-25 15:47:29 -0300

    Added randomize button

--- components/questions.jsx
+++ components/questions.jsx
@@ -19,15 +19,29 @@ export default function Questions() {
     console.log(calculateResult(selectedChoices))
   }
 
+  function randomize() {
+    const inputs = document.querySelectorAll('input');
+
+    for (let i = 0; i < inputs.length; i += 5) {
+      // Generate a random index between 0 and 4
+      const randomIndex = Math.floor(Math.random() * 5);
+      // Click the input at the random index within the current group of 5 inputs
+      inputs[i + randomIndex].click();
+    }
+    document.getElementById('button').scrollIntoView()
+  }
 
 
   return (
     <div className='max-w-screen-lg m-auto'>
+      <button className='bg-blue-700 text-blue-50 text-lg font-semibold px-8 py-2 rounded-md' onClick={randomize}>
+        RANDOM
+      </button>
       <form action="" onSubmit={handleSubmit}>
         <div className='space-y-8 divide-y'>
           {questionsList.map((question => <Question key={question.id} {...question} onChoiceChange={handleChoiceChange} />))}
         </div>
-        <div className='w-max mx-auto mt-4'>
+        <div className='w-max mx-auto mt-4' id='button'>
           <button type='submit' className='bg-blue-700 text-blue-50 text-lg font-semibold px-8 py-2 rounded-md'>
             SALVAR
           </button>
